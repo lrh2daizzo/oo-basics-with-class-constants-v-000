aa3361afe8e872693586db4fadd2a66bd4a64bfc
Luis Herrera <herrera.lrh@gmail.com>
2019-12-11 23:05:37 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,6 +4,7 @@ class Shoe
 
   BRANDS = []
 
+
   def initialize(brand)
     @brand = brand
     unless BRANDS.include?(brand)
@@ -12,7 +13,7 @@ class Shoe
   end
 
   def cobble
-    self.condition = "new"
+    @condition = "new"
     puts "Your shoe is as good as new!"
   end
 
